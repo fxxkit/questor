1181a03b6fc2212aabe255648aa272fe596794c3
Maxis Kao <maxiskao@yahoo-inc.com>
2015-05-21 00:49:02 +0800

    Add search-overlay

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -2,6 +2,19 @@ var searchIconClickHandler = function (e) {
 	$('.search-bar').focus();
 }
 
+var searchBoxFocusHandler = function (e) {
+	$('.search-overlay').addClass('mounted');
+}
+var searchBoxBlurHandler = function () {
+	$('.search-overlay').removeClass('mounted');
+}
+
+var searchOverlayClickHander = function () {
+	$('.search-overlay').removeClass('mounted');
+}
+
 function bindSearchEvents () {
 	$('.search-wrap').click(searchIconClickHandler);
+	$('.search-bar').focus(searchBoxFocusHandler).blur(searchBoxBlurHandler);
+	$('.search-overlay').click(searchOverlayClickHander);
 }

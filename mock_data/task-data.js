fcab56719817b7d3c537fddb62855092e9f31880
jordanhsu <jordanhsu@yahoo-inc.com>
2015-05-22 15:03:04 +0800

    refactor mock data to mock_data folder

---
+++ mock_data/task-data.js
@@ -0,0 +1,27 @@
+
+var _mock_nearbyTask = {
+    query_lat: 25.0562402,
+    query_lng: 121.6241145,
+    nearbyTaskIdList: [1,2,3,4,5,6,7,8,9]
+};
+
+var _mock_myTask = {
+    uid: "mock uid",
+    taskList: [
+        {
+            title: "HAHAHA_TASK",
+            type: "running"        
+        },
+        {
+            title: "BABABA_TASK",
+            type: "poopoo"        
+        },
+        {
+            title: "GGG_TASK",
+            type: "sockmydxxk"        
+        }
+    ]    
+};
+
+module.exports._mock_myTask = _mock_myTask;
+module.exports._mock_nearbyTask = _mock_nearbyTask;

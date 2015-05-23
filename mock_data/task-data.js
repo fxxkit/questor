361c548beef2b9f5a7572ef3115db4eaabcfe151
Maxis Kao <maxiskao@yahoo-inc.com>
2015-05-23 19:19:43 +0800

    refactor the data structure of mock data

--- mock_data/task-data.js
+++ mock_data/task-data.js
@@ -1,27 +1,60 @@
 
 var _mock_nearbyTask = {
     query_lat: 25.0562402,
-    query_lng: 121.6241145,
-    nearbyTaskIdList: [1,2,3,4,5,6,7,8,9]
+    query_lon: 121.6241145,
+    nearbyTaskIdList: [1, 3, 4]
 };
 
 var _mock_myTask = {
     uid: "mock uid",
-    taskList: [
-        {
-            title: "HAHAHA_TASK",
-            type: "running"        
-        },
-        {
-            title: "BABABA_TASK",
-            type: "poopoo"        
-        },
-        {
-            title: "GGG_TASK",
-            type: "sockmydxxk"        
-        }
-    ]    
+    myTaskIdList: [1, 2, 3]
 };
 
-module.exports._mock_myTask = _mock_myTask;
+var _mock_allTasks = [
+    {
+        title: "HAHAHA",
+        type: "running",
+        lat: "25.054737",
+        lon: "121.607484",
+        taskId: 1
+    },
+    {
+        title: "BABABA",
+        type: "poopoo",
+        lat: "25.1582402",
+        lon: "121.7141145",
+        taskId: 2
+    },
+    {
+        title: "GGG",
+        type: "sockmydxxk",
+        lat: "25.057969",
+        lon: "121.613268",
+        taskId: 3
+    },
+    {
+        title: "yellowcenter",
+        type: "poopoo",
+        lat: "25.05066",
+        lon: "121.619577",
+        taskId: 4
+    },
+    {
+        title: "pfpppp",
+        type: "running",
+        lat: "25.053493",
+        lon: "121.601089",
+        taskId: 5
+    },
+    {
+        title: "holisong",
+        type: "poopoo",
+        lat: "25.069509",
+        lon: "121.612762",
+        taskId: 6
+    }
+]
+
+module.exports._mock_myTask     = _mock_myTask;
 module.exports._mock_nearbyTask = _mock_nearbyTask;
+module.exports._mock_allTasks   = _mock_allTasks;

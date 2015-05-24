64b33af2fabcd6a50a8fdbf0f6c45868e4ba1ab1
jordanhsu <jordanhsu@yahoo-inc.com>
2015-05-24 19:27:46 +0800

    revise after code review

--- datamodel/my-mongo.js
+++ datamodel/my-mongo.js
@@ -2,23 +2,6 @@ var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var url = "mongodb://questor:iloveu@ds053638.mongolab.com:53638/questor_db";
 
-// Use connect method to connect to the Server
-// var db_connection = MongoClient.connect(url, function (err, db) {
-//   if (err) {
-//     console.log('Unable to connect to the mongoDB server. Error:', err);
-//   } else {
-//     console.log('Connection established to', url);
-
-
-
-
-//     //module.exports.db = db;
-
-
-//   }
-// });
-
-
 var dataAccess = {
 
     // tasks : single data or array list of data

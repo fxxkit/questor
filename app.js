e7081899b655332ee39a696b8ad61f392530ff75
jordanhsu <jordanhsu@yahoo-inc.com>
2015-05-25 00:50:43 +0800

    complete dataAccess for @GET /api/tasks/<taskId>

--- app.js
+++ app.js
@@ -80,58 +80,58 @@ app.get('/api', function (req, res) {
     @GET /api/tasks/<taskId>
 */
 app.get('/api/tasks/:_id([0-9]{1,8})', function (req, res) {
-    var taskId = req.params._id;
     debug('get task id:', taskId);
-    res.send(mock_data._mock_allTasks[taskId]);
+    var taskId = req.params._id;
+
+    var mock = req.query.mock;
+    if(mock){
+        res.send(mock_data._mock_allTasks[taskId]);
+    }
+    else{
+        db.dataAccess.getTask(taskId, function(err, data){
+            if(!err){
+                res.send(data);
+            }
+            else{
+                res.send("");
+            }
+        })    
+    }
 });
 
 /*
     @POST /api/tasks
 */
 app.post('/api/tasks',function  (req,res) {
-    var testData = [{
-        "name": "Rose", 
-        "title": "depurative and infrabranchial", 
-        "taskId": 5, 
-        "lat": 25.05964, 
-        "lng": 121.652214, 
-        "type": "Washoan"
-        },
-        {
-        "name": "Ronalda", 
-        "title": "semibalked and asomatous", 
-        "taskId": 0, 
-        "lat": 25.05674, 
-        "lng": 121.596615, 
-        "type": "Waaaaaaxxxxxxxllach"
-    }];
-    db.dataAccess.insertTasks(testData,function(data){
-        console.log(data);
+    var testData = mock_data._mock_allTasks;
+    db.dataAccess.insertTasks(testData,function(err, data){
+        //console.log(data);
         res.send(data);
     });
 });
 
 /*
     @GET /api/tasks/near?nrthEstLat=25.0562402&nrthEstLng=121.6241145&sthWstLat=25.0562402&sthWstLng=121.6241145
-        nrthEstLat : North East border lat
-        nrthEstLng : North East border lng
-        sthWstLat  : South West border lat
-        sthWstLng  : South West border lng
 */
 app.get('/api/tasks/near', function (req, res){
     var nrthEstLat = req.query.nrthEstLat;
     var nrthEstLng = req.query.nrthEstLng;
     var sthWstLat = req.query.sthWstLat;
     var sthWstLng = req.query.sthWstLng;
+    var resData = null;
 
     debug(req.query);
-
-    // collect nearby tasks by filtering taskId in allTasks
-    // var nearbytasks =  mock_data._mock_nearbyTask.nearbyTaskIdList.map(function (taskId, idx) {
-    //    return mock_data._mock_allTasks[idx];
-    // });
-    var nearbytasks = mock_data._mock_allTasks;
-    res.send(nearbytasks);
+    var mock = req.query.mock;
+    if(mock){
+        //The data in _mock_nearbyTask
+        resData =  mock_data._mock_nearbyTask.nearbyTaskIdList.map(function (taskId, idx) {
+           return mock_data._mock_allTasks[idx];
+        });    
+        //All mock data
+        // resData = mock_data._mock_allTasks;
+    }
+    
+    res.send(resData);
 });
 
 /*
--- datamodel/my-mongo.js
+++ datamodel/my-mongo.js
@@ -11,24 +11,45 @@ var dataAccess = {
                 console.log('Unable to connect to the mongoDB server. Error:', err);
             }
             else{
-                console.log("Insert data into collection task");
+                console.log("Insert data into task collection");
                 var collection = db.collection('task');
                 collection.insert(tasks, function (err, result){
                     if (err) {
                         console.log(err);
                         //return err;
-                        callback({
-                            status: 0,
-                            msg: err
-                        })
+                        callback(err,"");
                     } 
                     else {
                         console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
-                         callback({
-                            status: 1,
-                            msg: "OK"
-                        });
+                         callback("","OK");
                     }
+                    db.close();
+                });
+            }
+        });
+    },
+
+    getTask: function (req_taskId, callback){
+        req_taskId = parseInt(req_taskId);
+        MongoClient.connect(url, function(err, db){
+            if(err){
+                console.log('Unable to connect to the mongoDB server. Error:', err);
+            }
+            else{
+                console.log(" = get task from task collection = ");
+                var collection = db.collection('task');
+
+                //db.task.find( {taskId : 1 }  ).
+                collection.findOne({"taskId": req_taskId}, function (err,result){
+                    if(err){
+                        console.log(err);
+                        callback(err, "");
+                    }
+                    else{
+                        console.log(result);
+                        callback("", result);
+                    }
+                    db.close();
                 });
             }
         });

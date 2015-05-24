ff4f6177f74fe1c2a18222b336480e4824a90f76
jordanhsu <jordanhsu@yahoo-inc.com>
2015-05-25 01:31:00 +0800

    refactor db connection as method

--- datamodel/my-mongo.js
+++ datamodel/my-mongo.js
@@ -1,60 +1,70 @@
-var mongodb = require('mongodb');
+var mongodb = require('mongodb'),
+    debug = require('debug')('dataAccess');
+
 var MongoClient = mongodb.MongoClient;
 var url = "mongodb://questor:iloveu@ds053638.mongolab.com:53638/questor_db";
 
 var dataAccess = {
 
-    // tasks : single data or array list of data
-    insertTasks : function (tasks, callback){
-        MongoClient.connect(url, function (err, db) {
-            if (err) {
-                console.log('Unable to connect to the mongoDB server. Error:', err);
-            }
-            else{
-                console.log("Insert data into task collection");
-                var collection = db.collection('task');
-                collection.insert(tasks, function (err, result){
-                    if (err) {
-                        console.log(err);
-                        //return err;
-                        callback(err,"");
-                    } 
-                    else {
-                        console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
-                         callback("","OK");
-                    }
-                    db.close();
-                });
-            }
+    /*
+    * @param tasks : single data or array list of data
+    */
+    insertTasks: function(tasks, callback){
+        var reqData = tasks,
+            dbAction = EnumDbAction.INSERT,
+            collectionName = "task";
+
+        this._doQuery(reqData,dbAction,collectionName,function(err,result){
+            callback(err,result);
         });
+
     },
 
     getTask: function (req_taskId, callback){
         req_taskId = parseInt(req_taskId);
+        var reqData = {"taskId": req_taskId},
+            dbAction = EnumDbAction.FIND_ONE,
+            collectionName = "task";
+
+        this._doQuery(reqData,dbAction,collectionName,function(err, result){
+            callback(err,result);
+        });
+    },
+
+    _doQuery: function(reqData, dbAction, collectionName ,callback){
         MongoClient.connect(url, function(err, db){
             if(err){
                 console.log('Unable to connect to the mongoDB server. Error:', err);
             }
             else{
-                console.log(" = get task from task collection = ");
-                var collection = db.collection('task');
-
-                //db.task.find( {taskId : 1 }  ).
-                collection.findOne({"taskId": req_taskId}, function (err,result){
+                var collection = db.collection(collectionName);
+                // var method = dbAction;
+                collection[dbAction](reqData , function (err, result){
                     if(err){
-                        console.log(err);
-                        callback(err, "");
+                        // console.log("[DB Error]");
+                        // console.log(err);
+                        debug("[DB Error]");
+                        debug(err);
+                        callback(err,"");
                     }
                     else{
-                        console.log(result);
-                        callback("", result);
+                        // console.log()
+                        debug("[DB Action Success!]");
+                        debug(result);
+                        callback("",result);
                     }
-                    db.close();
                 });
             }
         });
     }
 }
+
+var EnumDbAction = {
+    FIND_ONE: "findOne",
+    FIND: "find",
+    INSERT: "insert",
+}
+
 
 module.exports.dataAccess = dataAccess;
 
